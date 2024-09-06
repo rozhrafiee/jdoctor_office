d6242fc43e83be072af8936f90e7f93dada31b4b
rozhrafiee <rozhinarafiee014@gmail.com>
2024-09-06 13:26:42 +0430

    miow

--- jdoctor_office/settings.py
+++ jdoctor_office/settings.py
@@ -138,8 +138,3 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication',
         ),
 }
-
-SIMPLE_JWT={
-    "ACCESS_TOKEN_LIFETIME": timedelta(hours=5),
-    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
-}

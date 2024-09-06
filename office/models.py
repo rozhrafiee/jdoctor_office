3a513b83592ce656d70047a3743e9c85800758a0
rozhrafiee <rozhinarafiee014@gmail.com>
2024-09-06 12:43:11 +0430

    models change

--- office/models.py
+++ office/models.py
@@ -6,11 +6,16 @@ class Transaction(models.Model):
     description = models.CharField(max_length=50)
 
 class Services(models.Model):
-    service_name = models.CharField(max_length=50)    
+    service_name = models.CharField(max_length=50)
+    price = models.PositiveBigIntegerField()    
     
 class Patient(models.Model):
     name = models.CharField(max_length=50)
     illness = models.CharField(max_length=60)
     required_service = models.ForeignKey(Services, on_delete=models.CASCADE)
 
+class Appointment(models.Model):
+    patient = models.ForeignKey(Patient, on_delete=models.CASCADE)
+    date = models.DateTimeField()
+    transaction = models.ForeignKey(Transaction, on_delete=models.CASCADE)
 
--- office/views.py
+++ office/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http.response import HttpResponse, JsonResponse, HttpResponseBadRequest
+from office.models import Transaction, Patient, Services, Appointment
+from rest_framework.generics import (
+    ListAPIView,
+    RetrieveAPIView,
+    CreateAPIView,
+    DestroyAPIView,
+    UpdateAPIView,
+    ListCreateAPIView,
+    RetrieveUpdateDestroyAPIView,
+)
+from rest_framework.permissions import IsAdminUser, IsAuthenticated
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import filters
 
-# Create your views here.
